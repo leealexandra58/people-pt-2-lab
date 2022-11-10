a3ee0ba4d725bb61be73033fbd326f0a7fd824cd
leeal <leealexandra58@gmail.com>
2022-11-10 16:51:36 -0500

    first commit unfinished

---
+++ people.js
@@ -0,0 +1,8 @@
+db.people.insertOne({ first_name: 'Allie', last_name: 'Lee', email: 'leeallie58@gmail.com', gender: 'Female', age: 32, state: 'Michigan', children: [] })
+db.people.insertOne({ first_name: 'Melissa', last_name: 'Lee', email: 'mellee87@gmail.com', gender: 'Female', age: 35, state: 'Incheon', 'children': [{name: "Audrey", age: 6}, {name: "Isaac", age: 4}] })
+db.people.updateOne({ first_name: 'Clarence' }, { $set: { state: 'South Dakota' } })
+db.people.updateOne({ first_name: 'Rebecca' }, { $unset: { email: 1 } })
+db.people.updateMany({ state: 'Missouri' }, { $inc: { age: 1 } })
+db.people.replaceOne({first_name: 'Jerry', last_name: 'Baker'}, { first_name: "Jerry", last_name: "Baker-Mendez", email: "jerry@classic.ly", gender:"Male", age: 28, state: "Vermont", "children": [{name: "Alan", age: 18}, {name: "Jenny", age: 3}] })
+db.people.deleteOne({ first_name: 'Wanda' })
+db.people.deleteMany({ email: null })
